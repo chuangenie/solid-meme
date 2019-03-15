172eda9edbf4da1fbc2b09481de11aba65e372a6
NG <15271906963@163.com>
2019-03-15 17:36:11 +0800

    项目初始化完成

--- src/main.js
+++ src/main.js
@@ -3,7 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
+import '@/styles/index.scss'
+// 引用element-ui
+Vue.use(Element)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
